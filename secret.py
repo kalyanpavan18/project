1982ccb083e7a3af58b72b39ded37e0e270bad5f
s.pavan kalyan <79745497+kalyanpavan18@users.noreply.github.com>
2023-03-30 23:22:16 +0530

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,4 @@
+access_token = "1094926398-xjds9JDzgcoKpov7AqZQB1OSOa83P6Ot1ISf5PQ"
+access_token_secret ="EdMgr9HC6IIXYgwUp2VDxfrEtgfdbiLe1ziA7HeChoIsG" 
+consumer_key ="UaPhFj3tRVHEzrKkjkIT32HFb" 
+consumer_secret ="UrUCOkdmPrdNhdG4inCBZvhfDUoHZOD7x0VL9HWzCBPAcpFIaM" 
